178b74a49cc101ae06314557fa425cd53a6d4732
Marie-Isabelle Schonborn <marieisabelleschoenborn@gmail.com>
2020-06-30 15:42:50 +0100

    map in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_151741) do
+ActiveRecord::Schema.define(version: 2020_06_30_111617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "producers", force: :cascade do |t|
+    t.string "name"
+    t.integer "phone_number"
+    t.string "address"
+    t.string "email"
+    t.text "description"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
