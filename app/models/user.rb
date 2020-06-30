6afcc154f5727c86028bd36e1444c6eded5bae4d
Ana Goncalves <alsgoncalves91@gmail.com>
2020-06-30 18:07:32 +0100

    user profile in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :role, inclusion: {in: %w(customer producer admin)}
+  has_one_attached :photo
 
+  validates :role, inclusion: { in: %w(customer producer admin) }
 end
