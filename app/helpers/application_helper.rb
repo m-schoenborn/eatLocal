3c28923461bfe9c808db3f2a297ec85c43bcf01b
Ana Goncalves <alsgoncalves91@gmail.com>
2020-07-01 18:10:00 +0100

    navbar improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
       image_tag('default_avatar.png', class: "avatar dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
     end
   end
+
+  def user_image(user)
+    if user.photo.attached?
+      cl_image_tag(current_user.photo.key, transformation: [
+        { width: 800, height: 800, gravity: 'face', radius: 'max', crop: 'fill' }
+      ], class: "avatar-large")
+    else
+      image_tag('default_avatar.png', class: "avatar-large")
+    end
+  end
 end
