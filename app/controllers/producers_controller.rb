757fdb0e3efa97de1f5e92b92fdf538ca3b39a95
Brian Corpas <briancorpas@gmail.com>
2020-07-01 13:01:32 +0100

    added actions to producer controller

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -15,4 +15,31 @@ class ProducersController < ApplicationController
   def show
     @producer = Producer.find(params[:id])
   end
+
+  def new
+    @producer = Producer.new
+    current_user.role == 'admin'
+  end
+
+  def create
+    @producer = Producer.new(producer_params)
+    current_user.role = 'admin'
+    @producer.save
+    redirect_to admin_dashboard_path
+  end
+
+  def edit
+    current_user.role = 'admin'
+  end
+
+  def update
+    current_user.role = 'admin'
+    @producer.update(producer_params)
+  end
+
+  private
+
+  def producer_params
+    params.require(:producer).permit(:name, :phone_number, :address, :email, :description)
+  end
 end
