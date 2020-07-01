3a85a1774c1d817248dec3f8e8050eb4215c9d10
Brian Corpas <briancorpas@gmail.com>
2020-07-01 17:00:45 +0100

    users added

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,18 +2,13 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+
 
     # Here is an example of a simple dashboard with columns and panels.
 
     columns do
       column do
-        panel " Recent Registered Producers" do
+         panel " Recent Registered Producers" do
           table_for Producer.order("id desc").limit(5) do
             column("Name") { |producer| (producer.name) }
             column("Address") { |producer| (producer.address) }
@@ -22,9 +17,8 @@ ActiveAdmin.register_page "Dashboard" do
           end
         end
       end
-
       column do
-        panel "Recent Users" do
+         panel "Recent Users" do
           table_for User.order("id desc").limit(10).each do |_user|
             column(:email)    { |user| link_to(user.email, admin_user_path(user)) }
           end
@@ -32,6 +26,7 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
     # end of columns
+
 
 
 
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -9,10 +9,10 @@ ActiveAdmin.register User do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :role, :first_name, :last_name]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+   permit_params do
+     permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :role, :first_name, :last_name]
+     permitted << :other if params[:action] == 'create' && current_user.admin?
+     permitted
+   end
+
 end
