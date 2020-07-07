06eb63451286d34580540ed51108405af427b452
Brian Corpas <briancorpas@gmail.com>
2020-07-07 12:16:17 +0100

    added producer_id to chatroom table

--- db/migrate/20200707111504_add_producer_id_to_chatroom.rb
+++ db/migrate/20200707111504_add_producer_id_to_chatroom.rb
@@ -1,5 +1,5 @@
 class AddProducerIdToChatroom < ActiveRecord::Migration[6.0]
   def change
-    add_column :chatrooms, :producer_id, :integer
+    add_column :chatrooms, :producer_id, :bigint
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_105252) do
+ActiveRecord::Schema.define(version: 2020_07_07_111504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_105252) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "producer_id"
+    t.bigint "producer_id"
   end
 
   create_table "favorites", force: :cascade do |t|
