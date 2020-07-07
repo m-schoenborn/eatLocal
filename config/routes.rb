80eaa24d61589de347bb6c834d31cf45c2f0d0d7
Brian Corpas <briancorpas@gmail.com>
2020-07-07 16:55:00 +0100

    erros controller created

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   get 'whatisinseason', to: 'pages#whatisinseason'
   get 'articles', to: 'pages#articles'
 
+  get '/404', to: "errors#not_found"
+  get '/422', to: "errors#unacceptable"
+  get '/500', to: "errors#internal_error"
+
   get "profile", to: "pages#profile", as: "profile"
 
   resources :chatrooms, only: :show do
