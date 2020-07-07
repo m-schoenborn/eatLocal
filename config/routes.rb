d9979815234a9786ff62e2c380212a9b503ea1b5
Marie-Isabelle Schonborn <marieisabelleschoenborn@gmail.com>
2020-07-07 17:23:06 +0100

    started producer profile page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'whatisinseason', to: 'pages#whatisinseason'
   get 'articles', to: 'pages#articles'
+  get 'producer_profile', to: 'pages#producer_profile'
 
   get "profile", to: "pages#profile", as: "profile"
 
