6b592dedd55c52a07e969ca648d4d3a9d29235ec
Brian Corpas <briancorpas@gmail.com>
2020-07-09 18:48:19 +0100

    seeds isuues

--- db/seeds.rb
+++ db/seeds.rb
@@ -112,12 +112,12 @@ user.photo.attach(io: file, filename: 'producer.jpg', content_type: 'image/jpg')
 
 Product.create(
   name: 'Award-winning free-range pork meat',
-  producer: Producer.find_by(name: 'Quinta da Bicuda',
+  producer: Producer.find_by(name: 'Quinta da Bicuda'),
   )
 
 ProductTag.create(
   product: Product.find_by(name: 'Award-winning free-range pork meat'),
-  tag: Tag.find_by(name: "Meat")
+  tag: Tag.find_by(name: "Meat"),
   )
 
 ProducerNews.create(
