3f3f898a856ce26dc61eb87aa34b6484664650c0
MaryJane <mj.lazaro8@gmail.com>
2020-07-10 11:08:09 +0100

    a litle change for the presentation

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -12,3 +12,14 @@ const initUpdateNavbarOnScroll = () => {
 }
 
 export { initUpdateNavbarOnScroll };
+
+$(document).on('click', function (e){
+  /* bootstrap collapse js adds "in" class to your collapsible element*/
+  var menu_opened = $('#navbarSupportedContent').hasClass('show');
+
+  if(!$(e.target).closest('#navbarSupportedContent').length &&
+      !$(e.target).is('#navbarSupportedContent') &&
+      menu_opened === true){
+          $('#navbarSupportedContent').collapse('toggle');
+  }
+});
